8b1a20ea185cd5c22a224888780a3564175b1c01
SAI TEJA H R <98202897+Saitejahr@users.noreply.github.com>
2023-05-01 23:53:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	if(n < 0) //Negative Number Edge Case
+      return -1;
+     
+    // Initialize result
+    let count = 0;
+ 
+    // Keep dividing n by powers of
+    // 5 and update count
+    for (let i = 5; Math.floor(n / i) >= 1; i *= 5)
+        count += Math.floor(n / i);
+ 
+    return count;
 }
 
 const input = prompt("Enter a number");
